b1b51a10413ef015c8f1ca8521a35516f5135728
pochipapa <p1xa1h68226768@gmail.com>
2026-06-12 20:08:20 +0900

    Add NeGconInput class definition and pin settings

--- headers/addons/negcon_input.h
+++ headers/addons/negcon_input.h
@@ -1,1 +1,27 @@
+#ifndef NEGCON_INPUT_H_
+#define NEGCON_INPUT_H_
 
+#include "gpaddon.h"
+#include "gamepad.h"
+
+// ネジコンのピン設定
+#define NEGCON_PIN_DAT 0
+#define NEGCON_PIN_CMD 1
+#define NEGCON_PIN_ATT 2
+#define NEGCON_PIN_CLK 3
+
+// システムのお約束（GPAddon）に従う宣言
+class NeGconInput : public GPAddon {
+public:
+    virtual bool available() override;
+    virtual void setup() override;
+    virtual void process() override;
+    virtual void preprocess() override {}
+    // ↓これが足りないと怒られていた「名前」の宣言です
+    virtual std::string name() override { return "NeGconInput"; }
+
+private:
+    uint8_t spi_transfer(uint8_t data);
+};
+
+#endif // NEGCON_INPUT_H_
